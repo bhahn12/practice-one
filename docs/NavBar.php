dd88d1780003f7f711b83a0548662e0ff5bde70c
bhahn12 <68400995+bhahn12@users.noreply.github.com>
2020-07-30 20:33:49 -0400

    new updates

---
+++ docs/NavBar.php
@@ -0,0 +1,5 @@
+<a href="index.php">Home</a> &nbsp;
+<a href="index.php?p=about">About Us</a> &nbsp;
+<a href="index.php?p=pricing">Pricing Chart</a> &nbsp;
+<a href="index.php?p=other">Other Services</a> &nbsp;
+<a href="index.php?p=contact">Contact Us</a>
---
+++ docs/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="stylesheet.css">
+        <meta charset="utf-8">
+
+    </head>
+    <h1>Bob's Lawn Cutting Service </h1><br></br>
+<script> src="NavBar.php"></script>
+        <img src="https://lh3.googleusercontent.com/Lvq_iQJlh5P28iusP7wnFeA0TgUuDe-Tty4bcyuq6lyo9AOruFsSxu_gnK92hItig2VKRQl5yHKeUahQ3BexmPCGg4u6dcXwTGbHBdwE8Ky4WH5GXGKcAdRN4K6CWJdvpF4v_HAf=w333-h151-p-k" alt='fresh lawn' class = "center">
+        </header>
+    <body>
+        <p>Are you too busy at work to maintain your lawn? <br></br>
+           Do you want to spend your free time with friends and family instead of cutting grass?<br></br>
+           My team and I have been cutting lawns in the Mercer County area for the last 25 years .
+           We are offering our services  </p> 
+    </body>
+</html>
+
+
+<!-- have drop down menu, about us, contact us, lawn table w pricing, calander w timeslots
+make sure once a sot is filled its greyed out, other services in table?, table should link
+back to calander when choosing price/size.  -->
